0368702a2cc3d899cf59fcacfb682529c91413a1
pymhd <aram808@gmail.com>
2018-07-03 18:39:24 +0300

    gofmt

--- types.go
+++ types.go
@@ -17,22 +17,4 @@ type HealthResponse struct {
 	Max      int   `json:"max"`
 }
 
-/*
-type Conductor chan interface{}
 
-
-type Request struct {
-	Key    string      `json:"key"`
-	Value  interface{} `json:"value"`
-	Action string      `json:"method"` // put, get, in
-	TTL    int  	   `json:"ttl"`
-	Pipe   Conductor
-}
-
-type Reply struct {
-	Error  bool
-	Result interface{}
-	Reason string
-}
-
-*/
